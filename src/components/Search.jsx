10982e67d7a3690dfba4c2c32a2dab2959deec20
jose andres <contretaa@gmail.com>
2022-03-24 17:20:32 -0400

    change: onChange changed for onKeyDown

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,21 @@
-const Search = ({ searchBooks }) => {
-  const handleChange = (evt) => {
-    const value = evt.target.value;
-    searchBooks(value);
+import { useLocation } from "wouter";
+
+const Search = () => {
+  const [_, setLocation] = useLocation();
+
+  const handleKeyDowm = (evt) => {
+    const search = evt.target.value;
+    if (evt.key === "Enter") {
+      setLocation(`/search/${search}`);
+    }
   };
+
   return (
     <input
       type="search"
       placeholder="Search Books"
       autoFocus
-      onChange={handleChange}
+      onKeyDown={handleKeyDowm}
     />
   );
 };
