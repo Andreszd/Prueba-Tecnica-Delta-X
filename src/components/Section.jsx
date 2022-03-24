4f7b178cbabbe5e8763d774433d8574bff3f2e66
jose andres <contretaa@gmail.com>
2022-03-24 12:28:12 -0400

    add: section component

---
+++ src/components/Section.jsx
@@ -0,0 +1,16 @@
+import Card from './Card';
+
+const BooksSection = ({ title, books, updateShelf }) => {
+  return (
+    <section className="section">
+      <h2 className="section__title">{title}</h2>
+      <div className="section__body l-flex-cards">
+        {books.map((book) => (
+          <Card key={book.id} book={book} updateShelf={updateShelf} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default BooksSection;
