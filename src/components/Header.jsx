cf64883fd724d3f707d5f8ea0956fc8a30e9e471
jose andres <contretaa@gmail.com>
2022-03-24 12:27:47 -0400

    add: header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import Search from './Search';
+
+const Header = ({ children }) => (
+  <header className="header">
+    <a src="#">Books</a>
+    {children}
+  </header>
+);
+
+export default Header;
